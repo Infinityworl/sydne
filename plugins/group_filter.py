891553a0e2122906447fe74f66848fa0b0e2047e
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-06-29 00:02:27 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -100,7 +100,7 @@ async def next_page(bot, query):
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'files#{nxreq}#{file.file_id}'),
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'files#{nxreq}#{file.file_id}')] for file in files ]
 
-    btn.insert(0, [InlineKeyboardButton("▫️️ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ▫️️", url='https://t.me/FilmZone_Official' )])
+    btn.insert(0, [InlineKeyboardButton("🌳 ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ 🌳",  url='https://telegram.me/infinity_lk')])
     if 0 < offset <= 10:
         off_set = 0
     elif offset == 0:
